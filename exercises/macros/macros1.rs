88de11d0e3a018e025371bc2f4af3e1668b14da8
Harris <47763936+Harris-pku@users.noreply.github.com>
2022-11-05 13:52:17 +0800

    Update macros1.rs

--- exercises/macros/macros1.rs
+++ exercises/macros/macros1.rs
@@ -1,18 +1,12 @@
-// modules1.rs
-// Execute `rustlings hint modules1` or use the `hint` watch subcommand for a hint.
+// macros1.rs
+// Execute `rustlings hint macros1` or use the `hint` watch subcommand for a hint.
 
-mod sausage_factory {
-    // Don't let anybody outside of this module see this!
-    fn get_secret_recipe() -> String {
-        String::from("Ginger")
-    }
-
-    pub fn make_sausage() {
-        get_secret_recipe();
-        println!("sausage!");
-    }
+macro_rules! my_macro {
+    () => {
+        println!("Check out my macro!");
+    };
 }
 
 fn main() {
-    sausage_factory::make_sausage();
+    my_macro!();
 }
